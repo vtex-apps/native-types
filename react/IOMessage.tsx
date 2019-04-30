d1974827627950b890bfab7576552dee51f3245d
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-04-30 19:25:22 -0300

    Remove defaultId

--- react/IOMessage.tsx
+++ react/IOMessage.tsx
@@ -2,17 +2,12 @@ import React from 'react'
 import { FormattedMessage, InjectedIntlProps, injectIntl } from 'react-intl'
 
 interface Props extends InjectedIntlProps {
-  defaultId?: string
   id: string
 }
 
-const IOMessage: React.FunctionComponent<Props> = ({ defaultId, id, intl }) => {
+const IOMessage: React.FunctionComponent<Props> = ({ id, intl }) => {
   if (intl.messages[id]) {
     return <FormattedMessage id={id} />
-  }
-
-  if (defaultId && intl.messages[defaultId]) {
-    return <FormattedMessage id={defaultId} />
   }
 
   return null
