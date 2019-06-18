31fc0b50fd6367cd72971cfbc0269f268319d1a8
Lucas Bebber <lucasbbebber@gmail.com>
2019-06-18 19:27:46 -0300

    Add support for react-intl child-as-function functionality

--- react/IOMessage.tsx
+++ react/IOMessage.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import { FormattedMessage, injectIntl } from 'react-intl'
 
 import { IOMessage as IOMessageType } from './typings/IOMessage'
 
-const IOMessage: IOMessageType = props => {
-  const intlMessage = props.intl.messages[props.id]
+const IOMessage: IOMessageType = ({ children, id, ...props }) => {
+  const intlMessage = props.intl.messages[id]
 
   if (intlMessage) {
-    return <FormattedMessage {...props} />
+    return <FormattedMessage id={id} {...props} />
   }
 
-  if (intlMessage === '') {
-    return null
+  if (children && typeof children === 'function') {
+    return children(intlMessage || id) as ReactElement || null
   }
 
-  return <>{props.id}</>
+  return !intlMessage ? null : <>{id}</>
 }
 
 export default injectIntl(IOMessage)
